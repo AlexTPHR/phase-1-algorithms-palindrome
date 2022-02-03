d57624449287df1ff8c6cfd0546a1c2bbdb68d0e
Alex <alex.chart@turningpointhr.com>
2022-02-03 20:17:37 +0000

    commit tree 1df23ea9f776ee988e29af0484145f319f22007d

--- index.js
+++ index.js
@@ -1,5 +1,47 @@
+// helper method
+// function reverseString(str) {
+//   // Step 1. Use the split() method to return a new array
+//   var splitString = str.split(""); // var splitString = "hello".split("");
+//   // ["h", "e", "l", "l", "o"]
+
+//   // Step 2. Use the reverse() method to reverse the new created array
+//   var reverseArray = splitString.reverse(); // var reverseArray = ["h", "e", "l", "l", "o"].reverse();
+//   // ["o", "l", "l", "e", "h"]
+
+//   // Step 3. Use the join() method to join all elements of the array into a string
+//   var joinArray = reverseArray.join(""); // var joinArray = ["o", "l", "l", "e", "h"].join("");
+//   // "olleh"
+  
+//   //Step 4. Return the reversed string
+//   return joinArray; // "olleh"
+// }
+
+// function isPalindrome(word) {
+//   // Write your algorithm here
+//   const reversedWord = reverseString(word)
+//   console.log (reversedWord)
+//   }
+
+function reverseString(word) {
+  // create an array from the input string
+  const wordArray = word.split("");
+  // reverse the array
+  const reversedWordArray = wordArray.reverse();
+  // create a string from the reversed array
+  const reversedWord = reversedWordArray.join("");
+  // return the reversed string
+  return reversedWord;
+}
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  // reverse the input string
+  const reversedWord = reverseString(word);
+  // if the reversed string is the same as the input
+  if (word === reversedWord) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /* 
